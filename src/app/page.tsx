5aaeb4a7d3b7487f74aeb5554dcfb0beabd2e19e
Zanges <3868054+Zanges@users.noreply.github.com>
2025-06-02 19:24:05 +0200

    Add header bar component with theme toggle and update layout in main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,17 @@ import {
   Card,
   CardContent
 } from "@/components/ui/card";
-import {ThemeToggle} from "@/components/theme-toggle";
 import {Chat} from "@/components/chat";
+import HeaderBar from "@/components/header-bar";
 
 export default function Home() {
   return (
-    <div className={'flex flex-col items-center justify-center h-screen'}>
-      <Card className={'w-11/12 shadow-lg flex flex-col flex-grow max-h-full overflow-hidden m-3'}>
+    <div className={'flex flex-col items-center justify-center h-screen bg-slate-200 dark:bg-slate-800'}>
+      <HeaderBar />
+      <Card className={'w-11/12 shadow-lg flex flex-col flex-grow max-h-full overflow-hidden m-3 p-0'}>
         <CardContent className="flex flex-col flex-grow p-2 overflow-hidden">
           <main className="flex flex-col flex-grow overflow-hidden">
-            <h1 className={'text-2xl font-bold mb-4'}>Welcome to My App</h1>
             <Chat/>
-            <div className="p-4 border-t">
-              <ThemeToggle />
-            </div>
           </main>
         </CardContent>
       </Card>
---
+++ src/components/header-bar.tsx
@@ -0,0 +1,10 @@
+import {ThemeToggle} from "@/components/theme-toggle";
+
+export default function HeaderBar() {
+  return (
+    <header className={'flex justify-between items-center w-full p-2 pl-4 pr-4 bg-slate-300 dark:bg-gray-900'}>
+      <h1 className={'text-2xl font-bold text-gray-900 dark:text-gray-100'}>Chat Application</h1>
+      <ThemeToggle/>
+    </header>
+  )
+}
